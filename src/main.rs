5a8af5d8bb53596f05ab16de78a96ac4a956141e
Krishna Shah <kshahusa@gmail.com>
2025-03-16 13:40:01 -0500

    ?

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,33 @@
 mod clipboard_manager;
 mod ui;
 
-use std::cell::RefCell;
-use std::thread;
+use std::sync::{Arc, Mutex};
 use std::sync::mpsc::{channel, Sender, Receiver};
+use std::thread;
 
 use arboard::Clipboard;
-
-use rdev::listen;
-
 use gtk::prelude::*;
 use gtk::{glib, Application};
+use rdev::listen;
 
 const APP_ID: &str = "org.gtk_rs.HelloWorld1";
 
 fn main() -> glib::ExitCode {
+    let (sender, receiver): (Sender<Vec<String>>, Receiver<Vec<String>>) = channel();
+    let clipboard_thread = thread::spawn(move || {
+        let mut stack: Vec<String> = Vec::new();
+        let mut clipboard = Clipboard::new().unwrap();
+        let mut ctrl_pressed = false;
+        if let Err(error) = listen(move |event| clipboard_manager::callback(event, &mut ctrl_pressed, &mut stack, &mut clipboard, sender.clone())) {
+            println!("Error: {:?}", error);
+        }
+    });
 
-    // let (sender, receiver): (Sender<Vec<String>>, Receiver<Vec<String>>) = channel();
-    // let clipboard_thread = thread::spawn(move || {
-    //     let mut stack: Vec<String> = Vec::new();
-    //     let mut clipboard = Clipboard::new().unwrap();
-    //     let mut ctrl_pressed = false;
-    //     if let Err(error) = listen(move |event| clipboard_manager::callback(event, &mut ctrl_pressed, &mut stack, &mut clipboard, sender.clone())) {
-    //         println!("Error: {:?}", error)
-    //     }
-    // });
-    // 
-    // loop {
-    //     let stack = receiver.recv().unwrap();
-    //     println!("Stack: {:?}", stack);
-    // }
-    
-    // let _ = clipboard_thread.join();
+    let receiver = Arc::new(Mutex::new(receiver));
     let app = Application::builder().application_id(APP_ID).build();
-    app.connect_activate(ui::build_ui);
+    app.connect_activate(move |app| {
+        ui::build_ui(app, Arc::clone(&receiver));
+    });
     app.run()
 }
-
-struct StaxyFlags {
-    receiver: Receiver<Vec<String>>
-}
-
-struct Staxy {
-    receiver: RefCell<Option<Receiver<Vec<String>>>>,
-    stack: Vec<String>
-}
-
 
--- src/ui.rs
+++ src/ui.rs
@@ -1,14 +1,40 @@
-use gtk::{Application, ApplicationWindow};
-use gtk::prelude::*;
 
+use gtk4::prelude::*;
+use gtk4::{glib, Application, ApplicationWindow};
+use std::sync::{Arc, Mutex};
+use std::sync::mpsc::Receiver;
+use std::cell::RefCell;
+use std::rc::Rc;
 
-pub fn build_ui(app: &Application) {
+pub fn build_ui(app: &Application, receiver: Arc<Mutex<Receiver<Vec<String>>>>) {
     // Create a window and set the title
     let window = ApplicationWindow::builder()
         .application(app)
         .title("My GTK App")
         .build();
 
+    // Create a label
+    let label = Label::new(None);
+    window.set_child(Some(&label));
+
+    let label_rc = Rc::new(RefCell::new(label));
+
+    // Add an idle source to the default main loop context
+    glib::timeout_add_local(std::time::Duration::from_millis(100), move || {
+        let receiver_clone = Arc::clone(&receiver);
+        // Lock the receiver to check for updates
+        if let Ok(receiver) = receiver_clone.lock() {
+            // Check if there are any updates from the receiver
+            if let Ok(stack) = receiver.try_recv() {
+                // Update the label with the new stack
+                label_rc.borrow().set_text(&format!("Stack: {:?}", stack));
+            }
+        }
+        // Continue calling this function
+        glib::Continue(true)
+    });
+
     // Present window
     window.present();
 }
+
